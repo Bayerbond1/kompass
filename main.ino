5070c950e154045e4eed678ea678c30b4706c2ba
André Kuhlmann <akuhltime@gmail.com>
2021-12-22 19:02:30 +0100

    Add Bluetooth Example Script
    - Incorporated the BLE example into the main.ino program
    - The ESP no connectes to the Application and is able to recieve a basic message from the peripheral (the iOS App)
    - Exported some of the sensor functionality into a seperate function

--- main.ino
+++ main.ino
@@ -8,6 +8,7 @@
 #include <ICM20948_WE.h>
 #include <FastLED.h>
 #include <BasicLinearAlgebra.h>
+#include "BLEDevice.h"
 using namespace BLA;
 
 #define CTRL_REG1 0x20
@@ -29,7 +30,7 @@ using namespace BLA;
 
 #define DEBUG 0
 
-//LEDs
+// LEDs
 #define AD0_VAL 1
 #define LED_DATA 18
 #define NUM_LEDS 16
@@ -39,6 +40,17 @@ CRGB leds[NUM_LEDS];
 #define ICM20948_ADDR 0x68
 ICM20948_WE myIMU = ICM20948_WE(ICM20948_ADDR);
 
+// BLUETOOTH
+static BLEUUID serviceUUID("60B8CB4F-49C8-44A9-8276-2B150F4303A3");
+static BLEUUID    charUUID("BD8A4CD3-E0F4-47D9-AA5B-27389E0E4C20");
+
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
+
+// VARIABLES
 float wantedHeading = 0.0;
 float gyro_x=0, gyro_y=0, gyro_z=0;  //value of gyro
 float gyro_x_init, gyro_y_init, gyro_z_init;   //offset of gyro
@@ -87,41 +99,111 @@ int disp = 0; //aid display function
 
 volatile byte new_gyro_num = 0;
 
-void setup(){
-  Wire.begin();
-  Serial.begin(115200);
-  if(!myIMU.init()){
-    Serial.println("ICM20948 does not respond");
-  }
-  else{
-    Serial.println("ICM20948 is connected");
-  }
-  //setupL3G4200D(2000); // Configure L3G4200  - 250, 500 or 2000 deg/sec
-  init_acc();
-  init_HMC5883();
-  calibrate();
-  //init_orientation();
-  read_acc();
-  update_acc(1);
-  delay(100);
-  read_acc();
-  update_acc(1);
-  magnometor_upadate(1);
-    
-  steps = micros();
-  last_acc=steps;
-  last_disp = steps;
-  last_gyro = steps;
-  last_mag=steps;
-  last_led=steps;
-  timer=0;
-  eular_angels_form_rotation_matrix(Orientation_Gyro);
-  disp_values();
-  FastLED.addLeds<WS2812B, LED_DATA, GRB>(leds, NUM_LEDS); // RGB, GRB, BGR
+// MARK: - BLUETOOTH
+
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+    Serial.print("Notify callback for characteristic ");
+    Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+    Serial.print(" of data length ");
+    Serial.println(length);
+    Serial.print("data: ");
+    Serial.println((char*)pData);
 }
 
-void loop() {
-  while(!new_gyro_num){check_data();}  //wait for new data
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {
+  }
+
+  void onDisconnect(BLEClient* pclient) {
+    connected = false;
+    Serial.println("onDisconnect");
+  }
+};
+
+bool connectToServer() {
+    Serial.print("Forming a connection to ");
+    Serial.println(myDevice->getAddress().toString().c_str());
+    
+    BLEClient*  pClient  = BLEDevice::createClient();
+    Serial.println(" - Created client");
+
+    pClient->setClientCallbacks(new MyClientCallback());
+
+    // Connect to the remove BLE Server.
+    pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+    Serial.println(" - Connected to server");
+    pClient->setMTU(517); //set client to request maximum MTU from server (default is 23 otherwise)
+  
+    // Obtain a reference to the service we are after in the remote BLE server.
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+      Serial.print("Failed to find our service UUID: ");
+      Serial.println(serviceUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    Serial.println(" - Found our service");
+
+
+    // Obtain a reference to the characteristic in the service of the remote BLE server.
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+    if (pRemoteCharacteristic == nullptr) {
+      Serial.print("Failed to find our characteristic UUID: ");
+      Serial.println(charUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    Serial.println(" - Found our characteristic");
+
+    // Read the value of the characteristic.
+    if(pRemoteCharacteristic->canRead()) {
+      std::string value = pRemoteCharacteristic->readValue();
+      Serial.print("The characteristic value was: ");
+      Serial.println(value.c_str());
+    }
+
+    if(pRemoteCharacteristic->canNotify())
+      pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+    connected = true;
+    return true;
+}
+/**
+ * Scan for BLE servers and find the first one that advertises the service we are looking for.
+ */
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+ /**
+   * Called for each advertising BLE server.
+   */
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.print("BLE Advertised Device found: ");
+    Serial.println(advertisedDevice.toString().c_str());
+
+    // We have found a device, let us now see if it contains the service we are looking for.
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+      doScan = true;
+
+    } // Found our server
+  } // onResult
+}; // MyAdvertisedDeviceCallbacks
+
+// -----------------
+
+// MARK: - FUNCTIONS
+
+void sensorLoop() {
+  //wait for new data
+  while(!new_gyro_num) {
+    check_data();
+  }
 
   myIMU.readSensor();
 
@@ -180,4 +262,72 @@ void new_gyro_data_avalible() //trigger this, when the interrupt happens
   new_gyro_num=1;
   delta_t=micros()-steps;
   steps=micros();
+}
+
+// -----------------
+
+void setup(){
+  Wire.begin();
+  Serial.begin(115200);
+
+  if(!myIMU.init()){
+    Serial.println("ICM20948 does not respond");
+  }
+  else{
+    Serial.println("ICM20948 is connected");
+  }
+  
+  //setupL3G4200D(2000); // Configure L3G4200  - 250, 500 or 2000 deg/sec
+  init_acc();
+  init_HMC5883();
+  calibrate();
+  //init_orientation();
+  read_acc();
+  update_acc(1);
+  delay(100);
+  read_acc();
+  update_acc(1);
+  magnometor_upadate(1);
+    
+  steps = micros();
+  last_acc=steps;
+  last_disp = steps;
+  last_gyro = steps;
+  last_mag=steps;
+  last_led=steps;
+  timer=0;
+  eular_angels_form_rotation_matrix(Orientation_Gyro);
+  disp_values();
+  FastLED.addLeds<WS2812B, LED_DATA, GRB>(leds, NUM_LEDS); // RGB, GRB, BGR
+
+  // BLUETOOTH
+  BLEDevice::init("");
+
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+}
+
+void loop() {
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are 
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer()) {
+      Serial.println("We are now connected to the BLE Server.");
+    } else {
+      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+    doConnect = false;
+  }
+
+  if (connected) {
+    sensorLoop();
+  }
 }
